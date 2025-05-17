290efc13a9adbd9c7afe82ad134bd2c974e0353c
BSIT-Sanchez <sanchezlando333@gmail.com>
2025-05-18 00:52:16 +0800

    added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,11 +8,11 @@ import {
     resetPassword,
     checkAuth,
 } from "../controllers/auth.controller.js";
-
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/check-auth", checkAuth);
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 router.post("/login", login);
